469d6cb4a7d4b240d799a926ea2d988eccf88ab8
Alessandro <alessandro.manca910@gmail.com>
2023-03-18 16:55:17 +0100

    -primo commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3977;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.status(200).send({msg: "Hola TinCode!"});
+});
+
+app.post("/welcome", (req, res) => {
+    const {username} = req.body;
+    res.status(200).send({msg: `Hola, ${username}`});
+});
+
+app.listen(PORT, () => {
+    console.log(`La tua porta è ${PORT}`);
+});
+
